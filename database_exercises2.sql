5c8fd90ea7be64da08d3fecb85c78087acbf26dd
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2020-03-10 13:35:44 -0700

    Add files via upload

--- database_exercises2.sql
+++ database_exercises2.sql
@@ -4,10 +4,10 @@ select * from PUBLISHERS
 
 select * from TITLES
 
-select * from TITLE_AUTHORS
-
 select * from ROYALTIES
 
 select * from AUTHORS where AU_LNAME like 'H%'
 
 select title_name, type, price, PUBDATE from TITLES natural join PUBLISHERS where PUBDATE between '07/15/2014' and '08/15/2014'
+
+select * from TITLES where TITLE_ID in ('T01', 'T04', 'T07')
