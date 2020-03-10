fd4fbc817c13712aa66c8d32af819d371ca9dd1f
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2020-03-10 13:40:22 -0700

    Add files via upload

--- database_exercises2.sql
+++ database_exercises2.sql
@@ -11,3 +11,7 @@ select * from AUTHORS where AU_LNAME like 'H%'
 select title_name, type, price, PUBDATE from TITLES natural join PUBLISHERS where PUBDATE between '07/15/2014' and '08/15/2014'
 
 select * from TITLES where TITLE_ID in ('T01', 'T04', 'T07')
+
+select AU_FNAME, AU_LNAME, ADDRESS, CITY, "STATE" from AUTHORS order by CITY, "STATE"
+
+
