6b492828a9cf457e0c982798805f2fa2a98a6bfd
Sergio Vasquez <30962111+sergiovasquez122@users.noreply.github.com>
2020-03-10 19:32:17 -0700

    Add files via upload

---
+++ database_3.sql
@@ -0,0 +1,12 @@
+select distinct CUSTOMERNAME, PRODUCTNAME from CUSTOMERS natural join ORDERS natural join ORDERDETAILS natural join PRODUCTS
+where QUANTITYORDERED >= 6
+
+select PRODUCTCODE, PRODUCTNAME, sum(100 * (MSRP - BUYPRICE) / BUYPRICE) as profit from PRODUCTS group by PRODUCTCODE, PRODUCTNAME having sum(100 * (MSRP - BUYPRICE) / BUYPRICE) > 125
+
+select COUNTRY, count(CUSTOMERNUMBER) as numberOfCustomers from CUSTOMERS group by COUNTRY order by count(CUSTOMERNUMBER) desc
+
+select distinct ORDERNUMBER, ORDERDATE, SHIPPEDDATE from ORDERS natural join ORDERDETAILS
+where ORDERDATE between '05/16/2014' and '06/7/2014'
+and SHIPPEDDATE between '05/20/2014' and '07/31/2014' order by ORDERDATE 
+
+select productName, productVendor from PRODUCTS where PRODUCTVENDOR like '%Gear%'
